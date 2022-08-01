730be2f6b1f61d91361761491aac170a03b398af
Pablo Martin Moreno <pablomartinmoreno@protonmail.com>
2022-07-31 22:44:55 -0300

    Agregado version con listas

--- 3.8.3 higher_multiplication.py
+++ 3.8.3 higher_multiplication.py
@@ -4,20 +4,21 @@ de dos de ellos. Por ejemplo, si recibe los números 1, 5, -2, -4 debe devolver 
 más grande que se puede obtener entre ellos (8 = −2 × −4).
 """
 
-def multiplication(number_one,number_two):
-    return (number_one * number_two)
+def higher_multiplication(number_one,number_two, number_three, number_four):
+    return max(number_one * number_two, number_one * number_three, number_one * number_four,
+            number_two * number_three, number_two * number_four, number_three * number_four)
 
-def higher_multiplication():
-    aux = 0 
-    max = 0
-    for i in range (0,5):
-        number_one = (int(input("numero 1 ")))     
-        number_two = (int(input("numero 2 ")))     
-        aux = multiplication(number_one,number_two)
-        if i == 0: 
-            max = aux
-        elif max < aux:
-            max = aux
-    return max
+print(higher_multiplication(-2,-8,-5,30))
 
-# print(higher_multiplication())
+# Version con listas 
+def higher_multiplication_list(number_one, number_two, number_three, number_four):
+    list_numbers = [number_one, number_two, number_three, number_four]
+    list_numbers.sort()  
+    if list_numbers[0] > 0:
+        return list_numbers[2] * list_numbers[3] 
+    elif list_numbers[3] < 0:
+        return list_numbers[0] * list_numbers[1]
+    else: 
+        return max(list_numbers[0] * list_numbers[1], list_numbers[2] * list_numbers[3])
+
+print(higher_multiplication_list(-2,-8,-5,30))
