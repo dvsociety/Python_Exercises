3ae6ad0d7dcd9117357248d2241716641e0ea240
Pablo Martin Morenos <pablomartinmoreno@protonmail.com>
2022-08-07 09:53:20 -0300

    Cambio minimo

--- 3.1 repeat_hello.py
+++ 3.1 repeat_hello.py
@@ -1,6 +1,6 @@
 """
- Ejercicio 3.1. Escribir una función repite_hola que reciba como parámetro un número entero
- n y escriba por pantalla el mensaje "Hola" n veces. Invocarla con distintos valores de n .
+Ejercicio 3.1. Escribir una función repite_hola que reciba como parámetro un número entero
+n y escriba por pantalla el mensaje "Hola" n veces. Invocarla con distintos valores de n .
 """
 def repeat_hello(n):
     aux = ""
