d9b29e59401dc56ddc852d68c28e39cabf6706df
dvsociety <pablomartinmoreno@protonmail.com>
2023-06-21 14:18:26 -0300

    Exercise improvement

--- 6.1_reverse_string.py
+++ 6.1_reverse_string.py
@@ -2,7 +2,23 @@
 Ejercicio 6.1. Escribir un ciclo que permita mostrar los caracteres de una cadena del final al
 principio.
 """
-def reverse_string():
-    sentence = "hola mundo!"
-    for count in range(1, len(sentence) + 1):
-        print(sentence[count - 1])
+sentence = "hola mundo!"
+
+def reverse_string(string_to_invert: str):
+    """_summary_
+
+    Args:
+        sentence (str): string to invert
+        
+    Returns: 
+        str: string invertido
+    """    
+    
+    new_string = ''
+
+    for i in string_to_invert: 
+        new_string = i + new_string
+    
+    return new_string
+
+print(reverse_string(sentence))
